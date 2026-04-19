f03dd03ca11e0b8583e1b298246d6979afec4401
Wellington Cardoso <wellingtoncardoso1514@gmail.com>
2026-04-19 18:32:22 -0300

    Criando DTO para comunicação segura entre a API e o cliente.

---
+++ BlazorProject.API/DTO/CreatePostDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorProject.API.DTO
+{
+    public class CreatePostDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Tag { get; set; } = string.Empty;
+        public string AuthorName { get; set; } = string.Empty;
+    }
+}
