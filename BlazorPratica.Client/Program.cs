9064e064c2616f496d70553294ca27ac39497df7
Wellington Cardoso <wellingtoncardoso1514@gmail.com>
2026-04-19 18:32:50 -0300

    Arquivos de teste

--- BlazorPratica.Client/Program.cs
+++ BlazorPratica.Client/Program.cs
@@ -13,5 +13,9 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri("http://localhost:5236/") // porta da minha API
 });
 
+builder.Services.AddHttpClient("JsonPlaceholder", client =>
+{
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+});
 
 await builder.Build().RunAsync();
